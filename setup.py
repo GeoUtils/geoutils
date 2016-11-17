348ab526a177d88c15a801a73e5567ce0a399a6c
”Amaury <amaury.dehecq@univ-savoie.fr>
2016-11-17 14:18:07 -0800

    Setup the Python packaging for easy sharing

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+#To prepare a new release
+#python setup.py sdist upload
+
+setup(name='geoutils',
+    version='1.0',
+    description='Libraries and command-line utilities for geospatial data processing/analysis',
+    author='Amaury Dehecq',
+    author_email='amaury.dehecq@jpl.nasa.gov',
+    license='MIT',
+      url='https://bitbucket.org/atedstone/geoutils.git',
+    packages=['geoutils'],
+    #long_description=open('README.md').read(),
+    #install_requires=['gdal','numpy','scipy','matplotlib'],
+    #Note: this will write to /usr/local/bin
+    scripts=['geoutils/geovector.py','geoutils/geometry.py','geoutils/demraster.py'])
+

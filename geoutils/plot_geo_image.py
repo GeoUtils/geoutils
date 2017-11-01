23af713c1c5359ad0ef207488463134b91fc465d
”Amaury <amaury.dehecq@univ-savoie.fr>
2017-11-01 11:32:06 -0700

    Major updates to plot_geo_image.py: - use argparse instead of the previous custom argument reading - add many more options to set image size, colorbar label, title, save to file etc... - work with multi bands

--- geoutils/plot_geo_image.py
+++ geoutils/plot_geo_image.py
@@ -5,90 +5,157 @@
 Description : Visualisation of any image that is compatible with GDAL
 
 Author : Amaury Dehecq
-Date : 02/2014
+Date : 11/2017
 """
 
 
 #Python libraries
-import os, sys
-import pylab as pl
-from matplotlib import cm
+from __future__ import print_function
+import os, sys, argparse
+import matplotlib.pyplot as plt
 import numpy as np
 
 #Personal libraries
-import georaster as geo
+import georaster as raster
 
 
 
 
 if __name__=='__main__':
 
-    # =============================================================================
-    def Usage():
-        print '*** Plot any image supported by GDAL ***'
-        print
-        print 'Usage : plot_raster.py filename colorbar=False cmap=jet'
-        print
-        print 'input parameters :'
-        print '  filename : str, path to the image file'
-        print 
-        print 'Opional arguments :'
-        print '  colorbar : True or False, display colorbar (Default is False)'
-        print '  cmap : matplotlib colormap (default is jet)'
-        print 
-# =============================================================================
+    #Set up arguments
+    parser = argparse.ArgumentParser(description='Visualisation tool for any image supported by GDAL.')
+
+    #Positional arguments
+    parser.add_argument('filename', type=str, help='str, path to the image')
+
+    #optional arguments
+    parser.add_argument('-cmap', dest='cmap', type=str, default='default', help='str, a matplotlib colormap string (default is from rcParams).')
+    parser.add_argument('-vmin', dest='vmin', type=str, default='default', help='float, the minimum value for colorscale (default is calculated min value).')
+    parser.add_argument('-vmax', dest='vmax', type=str, default='default', help='float, the maximum value for colorscale (default is calculated max value).')
+    parser.add_argument('-band', dest='band', type=int, default=0, help='int, which band to display (start at 0) for multiband images (Default is 0).')
+    parser.add_argument('-nocb', dest='nocb', help='If set, will not display a colorbar (Default is to display the colorbar).',action='store_true')
+    parser.add_argument('-clabel', dest='clabel', type=str, default='', help='str, the label for the colorscale (Default is empty).')
+    parser.add_argument('-title', dest='title', type=str, default='', help='str, figure title (Default is empty).')
+    parser.add_argument('-figsize', dest='figsize', type=str, default='default', help='str, figure size, must be a tuple of size 2, either written with quotes, or two numbers seperated by coma, no space (Default is from rcParams).')
+    parser.add_argument('-max_size', dest='max_size', type=int, default=2000, help='int, image size is limited to max_size**2 for faster reading/displaying (Default is 2000).')
+    parser.add_argument('-save', dest='save', type=str, default='', help='str, filename to the output filename to save to disk (Default is displayed on screen).')
+    parser.add_argument('-dpi', dest='dpi', type=str, default='default', help='int, dpi value to use when saving figure (Default is from rcParams).')
+    parser.add_argument('-nodata', dest='nodata', type=str, default='default', help='float, no data value (Default is read from file metadata).')
     
-    if len(sys.argv)<2:
-        Usage()
-        sys.exit(1)
+    args = parser.parse_args()
 
-    filename = sys.argv[1]
-    
-    #read optional arguments
-    kwargs = {}
-    items = ['colorbar','cmap','vmin','vmax']
-
-    kwargs['colorbar']=False
-    kwargs['cmap']='jet'
-
-    if len(sys.argv)>2:
-        for arg in sys.argv[2:]:
-    
-            item, value = arg.split('=')
-    
-            if item in items:
-                kwargs[item]=value
-
-    for key in kwargs.keys():
-        if kwargs[key]=='True':
-            kwargs[key] = True
-        elif kwargs[key]=='False':
-            kwargs[key] = False
-
-
-    img = geo.SingleBandRaster(filename)
-    data = img.r
+    ## Load image metadata ##
+    img = raster.MultiBandRaster(args.filename,load_data=False)
     xmin, xmax, ymin, ymax = img.extent
 
-    cmap = eval('cm.'+kwargs['cmap'])
-
-    #Resample if image is too large
-    ysize, xsize = data.shape
-    if xsize*ysize>2000*2000:
-        step = max(int(xsize/2000),int(ysize/2000))
-        data = data[::step,::step]
-
-    if kwargs.has_key('vmin')==False:
-        kwargs['vmin']=np.nanmin(data)
+    if args.nodata=='default':
+        nodata = img.ds.GetRasterBand(1).GetNoDataValue()
     else:
-        kwargs['vmin']=float(kwargs['vmin'])
+        try:
+            nodata = float(args.nodata)
+        except ValueError:
+            print("ERROR: nodata must be a float, currently set to %s" %args.nodata)
+            sys.exit(1)
 
-    if kwargs.has_key('vmax')==False:
-        kwargs['vmax']=np.nanmax(data)
+
+    ## Resample if image is too large ##
+    if img.nx*img.ny>args.max_size**2:
+        step = max(int(img.nx/args.max_size),int(img.ny/args.max_size))
+        print("Image will be downsampled by a factor %i." %step)
     else:
-        kwargs['vmax']=float(kwargs['vmax'])
+        step=1
 
-    pl.imshow(data,extent=(xmin,xmax,ymin,ymax),cmap=cmap,interpolation='none',vmin=kwargs['vmin'],vmax=kwargs['vmax'])
-    if kwargs['colorbar']==True:
-        pl.colorbar()
-    pl.show()
+    ## Read image ##
+    img = raster.MultiBandRaster(args.filename,downsampl=step,bands=(args.band+1,))
+    if nodata!=None:
+        data = np.ma.masked_array(img.r[:,:,0],mask=(img.r[:,:,0]==nodata))
+    else:
+        data = img.r[:,:,0]
+
+    ## Set default parameters ##
+
+    # vmin
+    if args.vmin=='default':
+        vmin=np.ma.min(data)
+    else:
+        try:
+            vmin=float(args.vmin)
+        except ValueError:
+            print("ERROR: vmin must be a float, currently set to %s" %args.vmin)
+            sys.exit(1)
+
+    # vmax
+    if args.vmax=='default':
+        vmax=np.ma.max(data)
+    else:
+        try:
+            vmax=float(args.vmax)
+        except ValueError:
+            print("ERROR: vmax must be a float, currently set to %s" %args.vmax)
+            sys.exit(1)
+
+    # color map
+    if args.cmap=='default':
+        cmap = plt.rcParams['image.cmap']
+    elif args.cmap in plt.cm.datad.keys():
+        cmap = args.cmap
+    else:
+        print("ERROR: cmap set to %s, must be in:" %args.cmap)
+        for i, elem in enumerate(plt.cm.datad.keys(), 1):
+            print(str(elem), end='\n' if i % 10 == 0 else ', ')
+        sys.exit(1)
+
+    # Figsize
+    if args.figsize=='default':
+        figsize = plt.rcParams['figure.figsize']
+    else:
+        print(eval(args.figsize))
+        print(tuple(eval(args.figsize)))
+        try:
+            figsize=tuple(eval(args.figsize))
+            xfigsize, yfigsize = figsize
+        except:
+            print("ERROR: figsize must be a tuple of size 2, currently set to %s" %args.figsize)
+            sys.exit(1)
+
+    # dpi
+    if args.dpi=='default':
+        dpi = plt.rcParams['figure.dpi']
+    else:
+        try:
+            dpi = int(args.dpi)
+        except ValueError:
+            print("ERROR: dpi must be an integer, currently set to %s" %args.dpi)
+            sys.exit(1)
+
+
+    
+    ## Plot data ##
+    
+    fig = plt.figure(figsize=figsize)
+
+    # plot
+    plt.imshow(data,extent=(xmin,xmax,ymin,ymax),cmap=cmap,interpolation='nearest',vmin=vmin,vmax=vmax)
+
+    # colorbar
+    if args.nocb==False:
+        cb = plt.colorbar()
+
+        if args.clabel!='':
+            cb.set_label(args.clabel)
+
+    # title
+    if args.title!='':
+        plt.title(args.title)
+
+    plt.tight_layout()
+
+    # Save
+    if args.save!='':
+        plt.savefig(args.save,dpi=dpi)
+        print("Figure saved to file %s." %args.save)
+    else:
+        print("Figure displayed on screen.")
+        plt.show()
+        

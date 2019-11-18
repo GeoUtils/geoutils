5215fa0ead1deda16ce43366bca1351085a67e21
Amaury Dehecq <amaury.dehecq@jpl.nasa.gov>
2019-11-18 10:12:58 -0800

    Modify method create_mask in geovector.py to take as argument the burn value used for the rasterization

--- geoutils/geovector.py
+++ geoutils/geovector.py
@@ -732,12 +732,13 @@ Additionally, a number of instances are available in the class.
 
 
     
-    def create_mask(self,raster='none',srs='none',xres='none',yres='none',extent='none'):
+    def create_mask(self,raster='none',srs='none',xres='none',yres='none',extent='none',burn_value=255):
         """
         Return a mask (array with dtype Byte) of the polygons in self.
         The spatial reference system of the mask can be set either :
         - by giving a georaster.__Raster object as input
         - by specifying the reference system srs, the raster pixel size (xres,yres) and the raster extent
+        - burn_value: float, the value to be burnt inside the polygons (Default is 255)
         """
 
         if raster=='none':
@@ -771,7 +772,7 @@ Additionally, a number of instances are available in the class.
         target_ds.SetProjection(srs.ExportToWkt())
 
         # Rasterize
-        err = gdal.RasterizeLayer(target_ds, [1], self.layer,burn_values=[255])
+        err = gdal.RasterizeLayer(target_ds, [1], self.layer,burn_values=[burn_value])
         if err != 0:
             raise Exception("error rasterizing layer: %s" % err)
 
